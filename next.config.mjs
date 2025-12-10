836488fce2adfa8e179fa2a4089c1deebbfbfeec
Diya Fathima <diya.f@codilar.com>
2025-12-10 21:05:11 +0530

    Update project with truck animation

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  experimental: {
+    disableRouteAnnouncer: true,   // ← THIS removes the N icon
+  },
 };
 
 export default nextConfig;
